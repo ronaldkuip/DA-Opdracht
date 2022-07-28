25b5b6d21d270415bb150f6c9ee97ce18c65260e
Ronald Kuip <ronald.kuip@live.nl>
2022-07-28 11:18:04 +0200

    Staging claculator file

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,10 +3,13 @@ import java.util.stream.DoubleStream;
 public class Calculator {
 
     static double add(double... operands) {
+        int a = 1;
+        a = 3;
         return DoubleStream.of(operands)
                 .sum();
-    }
 
+    }
+// barf
     static double multiply(double... operands) {
         return DoubleStream.of(operands)
                 .reduce(1, (a, b) -> a * b);
